6e82528de814cad433885c7bf3a4835339f7d4fd
TheKingDave <david@langheiter.com>
2020-03-03 14:21:09 +0100

    Implemented Sprechstunde

--- src/main/java/Sprechstunde.java
+++ src/main/java/Sprechstunde.java
@@ -1,5 +1,13 @@
 public class Sprechstunde {
 
+	public Sprechstunde(String lehrer, String raumnummer, int wochentag, String begin, String end) {
+		this.lehrer = lehrer;
+		this.raumnummer = raumnummer;
+		this.wochentag = wochentag;
+		this.begin = begin;
+		this.end = end;
+	}
+
 	private String lehrer;
 
 	private String raumnummer;
@@ -10,4 +18,23 @@ public class Sprechstunde {
 
 	private String end;
 
+	public String getLehrer() {
+		return lehrer;
+	}
+
+	public String getRaumnummer() {
+		return raumnummer;
+	}
+
+	public int getWochentag() {
+		return wochentag;
+	}
+
+	public String getBegin() {
+		return begin;
+	}
+
+	public String getEnd() {
+		return end;
+	}
 }
