72598412d4d7a7ae52e66b1b1cd5eb824ac0c441
TheKingDave <david@langheiter.com>
2020-03-03 14:49:55 +0100

    Finished tests

--- src/test/java/observerPush/SprechstundenListeTest.java
+++ src/test/java/observerPush/SprechstundenListeTest.java
@@ -2,22 +2,22 @@ package observerPush;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.*;
 
 public class SprechstundenListeTest {
 
     private SprechstundenListe sprechstundenListe;
-    private Sprechstunde dummy;
 
     @Before
     public void setUp() throws Exception {
         sprechstundenListe = new SprechstundenListe();
-        dummy = new Sprechstunde("", "", 0, "", "");
     }
 
     @Test
     public void addSprechstunde() {
+        Sprechstunde dummy = new Sprechstunde("", "", 0, "", "");
         assertEquals(0, sprechstundenListe.getList().size());
         sprechstundenListe.addSprechstunde(dummy);
         assertEquals(1, sprechstundenListe.getList().size());
@@ -26,6 +26,7 @@ public class SprechstundenListeTest {
 
     @Test
     public void removeSprechstunde() {
+        Sprechstunde dummy = new Sprechstunde("", "", 0, "", "");
         sprechstundenListe.addSprechstunde(dummy);
         assertEquals(1, sprechstundenListe.getList().size());
         sprechstundenListe.removeSprechstunde(dummy);
@@ -34,17 +35,35 @@ public class SprechstundenListeTest {
 
     @Test
     public void attach() {
+        Observer observer = mock(Observer.class);
+
+        sprechstundenListe.attach(observer);
+        sprechstundenListe.sendUpdate();
+
+        verify(observer).update(sprechstundenListe);
     }
 
     @Test
     public void remove() {
+        Observer observer = mock(Observer.class);
+
+        sprechstundenListe.attach(observer);
+        sprechstundenListe.remove(observer);
+        sprechstundenListe.sendUpdate();
+
+        verify(observer, never()).update(any());
     }
 
     @Test
     public void sendUpdate() {
-    }
+        Observer observer1 = mock(Observer.class);
+        Observer observer2 = mock(Observer.class);
 
-    @Test
-    public void getList() {
+        sprechstundenListe.attach(observer1);
+        sprechstundenListe.attach(observer2);
+        sprechstundenListe.sendUpdate();
+
+        verify(observer1).update(sprechstundenListe);
+        verify(observer2).update(sprechstundenListe);
     }
 }
