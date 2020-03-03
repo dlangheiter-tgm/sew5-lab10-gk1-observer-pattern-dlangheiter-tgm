84011b5a38bc61b59e354728a2f1d67ebc7ed3bf
TheKingDave <david@langheiter.com>
2020-03-03 14:28:46 +0100

    Implemented main

---
+++ src/main/java/observerPush/Main.java
@@ -0,0 +1,16 @@
+package observerPush;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SprechstundenListe sl = new SprechstundenListe();
+
+        sl.attach(new Email());
+        sl.attach(new WebService());
+
+        sl.addSprechstunde(new Sprechstunde("KOPG", "H932", 0, "11:30", "12:20"));
+        sl.addSprechstunde(new Sprechstunde("HOEK", "H933", 3, "9:50", "10:40"));
+        sl.sendUpdate();
+    }
+
+}
