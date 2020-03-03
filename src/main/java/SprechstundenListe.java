6aa6b69a3d7e5cbfc147f2c5623c45ac311723c6
TheKingDave <david@langheiter.com>
2020-03-03 14:23:59 +0100

    Implemented SprechstundenListe

--- src/main/java/SprechstundenListe.java
+++ src/main/java/SprechstundenListe.java
@@ -7,18 +7,18 @@ public class SprechstundenListe implements Subject {
 	private ArrayList<Sprechstunde> list;
 
 	public void addSprechstunde(Sprechstunde stunde) {
+		this.list.add(stunde);
 	}
 
 	public void removeSprechstunde(Sprechstunde stunde) {
-
+		this.list.remove(stunde);
 	}
-
 
 	/**
 	 * @see Subject#attach(Observer)
 	 */
 	public void attach(Observer observer) {
-
+		this.observers.add(observer);
 	}
 
 
@@ -26,7 +26,7 @@ public class SprechstundenListe implements Subject {
 	 * @see Subject#remove(Observer)
 	 */
 	public void remove(Observer observer) {
-
+		this.observers.remove(observer);
 	}
 
 
@@ -34,7 +34,9 @@ public class SprechstundenListe implements Subject {
 	 * @see Subject#sendUpdate()
 	 */
 	public void sendUpdate() {
-
+		for(Observer observer : observers) {
+			observer.update(this);
+		}
 	}
 
 	public ArrayList<Sprechstunde> getList() {
